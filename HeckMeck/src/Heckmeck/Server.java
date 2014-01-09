3a51da129c6571ef52448faeeaad4b894c7cd8ba
Marilena Magnasco <marilena.magnasco@zetvisions.com>
2014-01-09 09:56:11 +0100

    Korrektur

--- HeckMeck/src/Heckmeck/Server.java
+++ HeckMeck/src/Heckmeck/Server.java
@@ -6,30 +6,30 @@ public class Server  {
 	
 	public static void main(String[] args){
 		
-//		int playerCount = 2;
-		
-		String abc = "ABC";
-		String abd = "ABC";
-		
-		if (abc == abd){
-		
-		System.out.println( "Gleich" );
-		}else{
-			System.out.println( "Ungleich" );
-			
+		int playerCount = 2;
+				
+		if (args.length > 0){
+			try{
+			playerCount = new Integer(args[0]);
+			}	catch (NumberFormatException nfe){
+				
+			}
 		}
-		
-//		
-//		if (args.length > 0){
-//			try{
-//			playerCount = new Integer(args[0]);
-//			}	catch (NumberFormatException nfe){
-//				
-//			}
-//		}
 
-//		new Server( playerCount );
-		char a = 'A' ;
+		new Server( playerCount );
+		
+		
+//		String abc = "ABC";
+//		String abd = "ABC";
+//		
+//		if (abc == abd){
+//		System.out.println( "Gleich" );
+//		}
+//		else{
+//		System.out.println( "Ungleich" );
+//		}
+//		char a = 'A' ;
+		
 	}
 
 	private Server( int playerCount ){
