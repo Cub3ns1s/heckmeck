a740d8b335d477a6c29e0441ddcae153356ade27
Marilena Magnasco <marilena.magnasco@zetvisions.com>
2014-01-09 13:36:53 +0100

    Server Rumprobiererei

--- HeckMeck/src/Heckmeck/Server.java
+++ HeckMeck/src/Heckmeck/Server.java
@@ -1,20 +1,42 @@
 package Heckmeck;
 
-public class Server  {
+import java.io.IOException;
+import java.net.*;
+import java.lang.Thread;
+
+public class Server implements Runnable{
 
 	//Attributes
 	private int mPlayerCount;
-	
+	private ServerSocket mServerSocket;
+	private Thread mThread;
 	
 	//Konstruktor
-	private Server( int playerCount ){
+	private Server( int playerCount ) {
 		
 		System.out.println( "Starte Server für " + playerCount + " Spieler");
 //		Warten auf Anmeldung
+		for (int i = 0; i < playerCount; i++) {
+			
+		try {
+		mServerSocket = new ServerSocket();
+		mThread = new Thread(this);
+		mThread.start();
+		}
 		
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		}
+
 //		Spiel starten
 //		Game game = new Game( this );
 	}
+	
+	@Override
+	public void run() {
+
+	}	
 	
 	
 	public static void main(String[] args){
@@ -24,12 +46,11 @@ public class Server  {
 		if (args.length > 0){
 			try{
 			playerCount = new Integer(args[0]);
-			}	catch (NumberFormatException nfe){
-				
 			}
+			catch (NumberFormatException nfe){};
 		}
-
-		new Server( playerCount );
+		
+			new Server( playerCount );
+		}
 				
-	}	
-}
+	}
