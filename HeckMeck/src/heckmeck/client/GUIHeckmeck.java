04e6006c613b1406f80cc4449ce31b7392605ff1
Marilena Magnasco <marilena.magnasco@zetvisions.com>
2014-02-18 15:27:06 +0100

    aufhübschen

--- HeckMeck/src/heckmeck/client/GUIHeckmeck.java
+++ HeckMeck/src/heckmeck/client/GUIHeckmeck.java
@@ -11,69 +11,90 @@ import javax.swing.*;
 public class GUIHeckmeck extends JFrame {
 
 	private static final long serialVersionUID = 4220957201237157813L;
-	private JFrame mFenster;
+	private JFrame mFrame;
 	private GameState mGameState;
 
 	public GUIHeckmeck(GameState gameState) {
-		// erzeuge Fenster
 		mGameState = gameState;
 
-		mFenster = new JFrame("Heckmeck");
-		mFenster.setSize(Toolkit.getDefaultToolkit().getScreenSize());
-		mFenster.setResizable(false);
-		mFenster.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		mFenster.getContentPane().setBackground(new Color(122, 6, 39));
-		mFenster.setLayout(new BorderLayout());
-		mFenster.setVisible(true);
+		createFrame();
+		createTopPanel();
+		createBottomPanel();
+		createLeftPanel();
+		createRightPanel();
+		createCenterPanel();
+	}
 
-		JPanel pTop = new JPanel();
-		pTop.setBackground(Color.yellow);
-		pTop.setPreferredSize(new Dimension(1000, 300));
-		pTop.add(new JLabel("TOP"));
-		mFenster.add(BorderLayout.PAGE_START, pTop);
-		mFenster.revalidate();
-
-		JPanel pBottom = new JPanel();
-		pBottom.setBackground(Color.yellow);
-		pBottom.setPreferredSize(new Dimension(1000, 300));
-		pBottom.add(new JLabel("BOTTOM"));
-		mFenster.add(BorderLayout.PAGE_END, pBottom);
-		mFenster.revalidate();
-
-		JPanel pLeft = new JPanel();
-		pLeft.setBackground(Color.cyan);
-		pLeft.setPreferredSize(new Dimension(300, 1000));
-		pLeft.add(new JLabel("LEFT"));
-		mFenster.add(BorderLayout.LINE_START, pLeft);
-		mFenster.revalidate();
-
-		JPanel pRight = new JPanel();
-		pRight.setBackground(Color.cyan);
-		pRight.setPreferredSize(new Dimension(300, 1000));
-		pRight.add(new JLabel("RIGHT"));
-		mFenster.add(BorderLayout.LINE_END, pRight);
-		mFenster.revalidate();
-
+	private void createCenterPanel() {
 		JPanel pCenter = new JPanel(new GridBagLayout());
+		pCenter.setBackground(new Color(122, 6, 39));
+
+		insertGrillTokenImages(pCenter);
+
+		mFrame.getContentPane().add(pCenter);
+		mFrame.revalidate();
+	}
+
+	private void insertGrillTokenImages(JPanel pCenter) {
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.ipadx = 5;
 		gbc.ipady = 5;
-		pCenter.setBackground(new Color(122, 6, 39));
 
 		for (Iterator<Token> iterator = mGameState.getGrill().getTokens()
 				.iterator(); iterator.hasNext();) {
 			Token token = iterator.next();
 
-			String path = "L:/Ausbildung/Heckmeck/"
-					+ token.getValue() + ".png";
-			
+			String path = "L:/Ausbildung/Heckmeck/" + token.getValue() + ".png";
+
 			pCenter.add(new JLabel(new ImageIcon(path)), gbc);
-			mFenster.revalidate();
+			mFrame.revalidate();
 		}
+	}
 
-		mFenster.getContentPane().add(pCenter);
-		mFenster.revalidate();
+	private void createRightPanel() {
+		JPanel pRight = new JPanel();
+		pRight.setBackground(Color.cyan);
+		pRight.setPreferredSize(new Dimension(300, 1000));
+		pRight.add(new JLabel("RIGHT"));
+		mFrame.add(BorderLayout.LINE_END, pRight);
+		mFrame.revalidate();
+	}
 
+	private void createLeftPanel() {
+		JPanel pLeft = new JPanel();
+		pLeft.setBackground(Color.cyan);
+		pLeft.setPreferredSize(new Dimension(300, 1000));
+		pLeft.add(new JLabel("LEFT"));
+		mFrame.add(BorderLayout.LINE_START, pLeft);
+		mFrame.revalidate();
+	}
+
+	private void createBottomPanel() {
+		JPanel pBottom = new JPanel();
+		pBottom.setBackground(Color.yellow);
+		pBottom.setPreferredSize(new Dimension(1000, 300));
+		pBottom.add(new JLabel("BOTTOM"));
+		mFrame.add(BorderLayout.PAGE_END, pBottom);
+		mFrame.revalidate();
+	}
+
+	private void createTopPanel() {
+		JPanel pTop = new JPanel();
+		pTop.setBackground(Color.yellow);
+		pTop.setPreferredSize(new Dimension(1000, 300));
+		pTop.add(new JLabel("TOP"));
+		mFrame.add(BorderLayout.PAGE_START, pTop);
+		mFrame.revalidate();
+	}
+
+	private void createFrame() {
+		mFrame = new JFrame("Heckmeck");
+		mFrame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		mFrame.setResizable(false);
+		mFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		mFrame.getContentPane().setBackground(new Color(122, 6, 39));
+		mFrame.setLayout(new BorderLayout());
+		mFrame.setVisible(true);
 	}
 
 	public static void main(GameState[] args) {
